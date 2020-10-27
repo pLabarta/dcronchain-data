e93ef7b87fa452bf23ba51dfabb9d2da6970c935
pLabarta <pablitolabarta@gmail.com>
2020-10-27 16:26:16 -0300

    config and scripts

--- docker/generate_insights.py
+++ docker/generate_insights.py
@@ -29,20 +29,22 @@ df = df.set_index('date')
 df.to_csv(f'{repo_dir}/data/full_decred_data.csv')
 df.to_json(f'{repo_dir}/data/full_decred_data.json',orient='split')
 
-# Generate Chart Overview Table v1
+    # Generate Chart Overview Table v1
 # Details:
 #  - Market Gradient uses 28 days
-# import csv
-# columns='CapMVRVCur,Mayer_Multiple,Price_DCRBTC_Mid,MrktGradient,UnrealisedPnL_Net'.split(',')
-# metrics = []
-# for column in columns:
-#     metrics.append(ChartOverview(df,column))
 
-# with open('chart_overview_table.csv','w') as csvfile:
-#     writer = csv.writer(csvfile)
-#     writer.writerow(['name','value_current','pastvalue_1d','pastvalue_7d','ma28day'])
-#     for metric in metrics:
-#         writer.writerow([metric.name,metric.today,metric.yesterday,metric.past_week,metric.period28])
+columns='CapMVRVCur,Mayer_Multiple,PriceBTC,MrktGradient,UnrealisedPnL_Net'.split(',')
+metrics = []
+for column in columns:
+        metrics.append(ChartOverview(df,column))
+
+charts_df = pd.DataFrame()
+charts_df['name'] = [metric.name for metric in metrics]
+charts_df['today'] = [metric.today for metric in metrics]
+charts_df['yesterday'] = [metric.yesterday for metric in metrics]
+charts_df['past_week'] = [metric.past_week for metric in metrics]
+charts_df['28dayMA'] = [metric.period28 for metric in metrics]
+charts_df.to_json(f'{repo_dir}/data/homepage_charts_table.json',orient='table')    
 
 # Generate General Insights Table
 # Key Components:
@@ -81,7 +83,7 @@ power = df['PriceUSD']
 power_desc = 'Primary: Today Price, Secondary: Last Month Price, Statusbar: MonthlyChange%'
 power_primary=power[-1]
 power_secondary=power[-30]
-power_statusbar=(power_secondary-power_primary)/power_primary*100
+power_statusbar=(power_primary-power_secondary)/power_secondary*100
 power_insight = MetricInsight('Decred Power',power_primary,power_secondary,power_statusbar,power_desc)
 insight_list.append(power_insight)
 
@@ -90,7 +92,7 @@ realcap = df['CapRealUSD']
 realcap_desc = 'Primary: Today RealisedCapUSD, Secondary: LastMonth RealisedCapUSD, Statusbar: MonthlyChange%'
 realcap_primary = realcap[-1]
 realcap_secondary = realcap[-30]
-realcap_statusbar =(realcap_secondary-realcap_primary)/realcap_primary*100
+realcap_statusbar =(realcap_primary-realcap_secondary)/realcap_secondary*100
 realcap_insight = MetricInsight('Realised Cap',realcap_primary,realcap_secondary,realcap_statusbar,realcap_desc)
 insight_list.append(realcap_insight)
 
@@ -101,4 +103,39 @@ insight_df['secondary'] = [insight.secondary for insight in insight_list]
 insight_df['statusbar'] = [insight.statusbar for insight in insight_list]
 insight_df['description'] = [insight.description for insight in insight_list]
 
-insight_df.to_json(f'{repo_dir}/data/homepage_insights.json',orient='split')
+insight_df.to_json(f'{repo_dir}/data/homepage_insights.json',orient='table')
+
+# Generate Assorted Metric Table
+
+other_columns = 'PriceUSD,CapMrktCurUSD,tic_price_avg,tic_usd_cost,pow_hashrate_THs_avg,TxCnt,TxTfrValMeanNtv,AdrActCnt'.split(",")
+other_metrics = []
+
+for column in other_columns:
+        other_metrics.append(ChartOverview(df,column))
+
+other_metrics_df = pd.DataFrame()
+other_metrics_df['name'] = [metric.name for metric in other_metrics]
+other_metrics_df['today'] = [metric.today for metric in other_metrics]
+other_metrics_df['yesterday'] = [metric.yesterday for metric in other_metrics]
+other_metrics_df['past_week'] = [metric.past_week for metric in other_metrics]
+other_metrics_df['28dayMA'] = [metric.period28 for metric in other_metrics]
+
+other_metrics_df.to_json(f'{repo_dir}/data/homepage_metric_table.json',orient='table')
+
+# Generate Featured Chart Insights
+# Insights for Mayer Multiple Chart
+
+mayer_columns = '200DMA,Mayer_Multiple,PriceUSD'.split(",")
+mayer_insights = []
+
+for column in mayer_columns:
+    mayer_insights.append(ChartOverview(df,column))
+
+mayer_insights_df = pd.DataFrame()
+mayer_insights_df['name'] = [metric.name for metric in mayer_insights]
+mayer_insights_df['today'] = [metric.today for metric in mayer_insights]
+mayer_insights_df['yesterday'] = [metric.yesterday for metric in mayer_insights]
+mayer_insights_df['past_week'] = [metric.past_week for metric in mayer_insights]
+mayer_insights_df['28dayMA'] = [metric.period28 for metric in mayer_insights]
+
+mayer_insights_df.to_json(f'{repo_dir}/data/homepage_featured_chart_insights.json',orient='table')
